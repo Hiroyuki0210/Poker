e21b36f2e04abbe27e44458c3e3dc11a167ce89e
hiroyuki0210 <h.tamaki@atomitech.jp>
2019-06-10 16:00:56 +0900

    update

--- src/Judge/JudgeParam.java
+++ src/Judge/JudgeParam.java
@@ -6,42 +6,37 @@ import card.Card;
 import card.Suit;
 
 
-//JudgeMaterialクラスで用いる変数をセット、取得する
+//Judgeクラスで用いる変数をセット、取得する
 public class JudgeParam {
-	private int max = 0;
+	private int max ;
 	private Suit suit;
-	private int num = 0;
+	private int num ;
 	private List<Card> pairnum;
+
+	public JudgeParam(int max, Suit suit) {
+		this.max = max;
+		this.suit = suit;
+	}
+
+	public JudgeParam(int num, List<Card> pairnum) {
+		this.num = num;
+		this.pairnum = pairnum;
+	}
 
 	public int getMaxNum() {
 		return max;
-	}
-
-	public void setMaxNum(int max) {
-		this.max = max;
 	}
 
 	public Suit getMaxSuit() {
 		return suit;
 	}
 
-	public void setMaxSuit(Suit suit) {
-		this.suit = suit;
-	}
-
 	public int getNum() {
 		return num;
 	}
 
-	public void setNum(int num) {
-		this.num = num;
-	}
-
 	public List<Card> getPairNum() {
 		return pairnum;
-	}
-	public void setPairNum(List<Card> pairnum) {
-		this.pairnum = pairnum;
 	}
 
 }
