8043c5dab7b895d7c5c3d9b846c9dfc49b03c4f2
hiroyuki0210 <h.tamaki@atomitech.jp>
2019-07-09 16:52:07 +0900

    modify

--- src/main/java/judge/Judgement.java
+++ src/main/java/judge/Judgement.java
@@ -7,62 +7,50 @@ import card.Card;
 public class Judgement {
 
 	public static String result(List<Card> hand) {
-		String result = "";
-
-		String straight = "";
-		String flash = "";
-		String straightflash = "";
-		String royalstraightflash = "";
-		String pair = "";
-		String card = "";
-		String fullhouse = "";
-		String[] type = {straight, flash, straightflash, royalstraightflash, pair, card, fullhouse};
-
+		PokerHand result = PokerHand.NOTHING;
 		Yaku jud = new Yaku();
-
 		jud.changeA(hand);
 
 		if(5 <= jud.straight(hand).getMaxNum() && jud.straight(hand).getMaxNum() <= 14) {
-			type[0] = "ストレート";
+			result = PokerHand.STRAIGHT;
 		}
 
 	    if(jud.flash(hand) == 1){
-	    	type[1] = "フラッシュ";
+	    	result = PokerHand.FLASH;
 	    }
 
 	    if(5 <= jud.straight(hand).getMaxNum() && jud.straight(hand).getMaxNum() <= 14 && jud.flash(hand) == 1) {
-	    	type[0] = "";
-	    	type[1] = "";
-	    	type[2] = "ストレートフラッシュ";
+	    	result = PokerHand.STRAIGHTFLASH;
 		}
 
 	    if(jud.straight(hand).getMaxNum() == 14 && jud.flash(hand) == 1) {
-	    	type[2] = "";
-			type[3] = "ロイヤルストレートフラッシュ";
+	    	result = PokerHand.ROYALSTRAIGHTFLASH;
 		}
 
 		if(jud.pair(hand).getPairNum().size() != 0 && jud.pair(hand).getNum() != 3 && jud.pair(hand).getNum() != 4){
-			type[4] = String.valueOf(jud.pair(hand).getPairNum().size())+"ペア";
+			if(jud.pair(hand).getPairNum().size() == 1) {
+				result = PokerHand.ONEPAIR;
+			}
+			if(jud.pair(hand).getPairNum().size() == 2) {
+				result = PokerHand.TWOPAIR;
+			}
 		}
 
 		if((jud.pair(hand).getNum() == 3 || jud.pair(hand).getNum() == 4) && jud.pair(hand).getPairNum().size() == 1){
-			type[5] = String.valueOf(jud.pair(hand).getNum())+"カード";
+			if(jud.pair(hand).getNum() == 3) {
+				result = PokerHand.THREECARD;
+			}
+			if(jud.pair(hand).getNum() == 4) {
+				result = PokerHand.FOURCARD;
+			}
 		}
 
 		if(jud.pair(hand).getNum() == 3 && jud.pair(hand).getPairNum().size() == 2){
-			type[4] = "";
-			type[5] = "";
-			type[6] = "フルハウス";
-		}
-
-		for(int i =0; i<type.length; i++) {
-			if(! type[i].equals("")) {
-				result = type[i];
-			}
+			result = PokerHand.FULLHOUSE;
 		}
 
 		jud.returnA(hand);
 
-		return result;
+		return result.getName();
 	}
 }
--- src/test/java/JudgeTest.java
+++ src/test/java/JudgeTest.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.Test;
 import card.Card;
 import card.Suit;
 import judge.Judgement;
+import judge.PokerHand;
 
+//10種類の役のテスト
 class JudgeTest {
 
 	List<Card> hand = new ArrayList<>();
@@ -22,56 +24,72 @@ class JudgeTest {
 		hand.add(new Card(Suit.CLOVER,4));
 		hand.add(new Card(Suit.SPADE,5));
 		hand.add(new Card(Suit.CLOVER,6));
-		assertEquals(Judgement.result(hand),"ストレート");
+		assertEquals(Judgement.result(hand),PokerHand.STRAIGHT.getName());
 	}
 
 	@Test
 	void flash() {
-		for(int i=0; i <5 ; i++) {
-			hand.add(new Card(Suit.SPADE,i*2));
+		for(int i=0; i <5 ; i += 2) {
+			hand.add(new Card(Suit.SPADE,i));
 		}
-		assertEquals(Judgement.result(hand),"フラッシュ");
+		assertEquals(Judgement.result(hand),PokerHand.FLASH.getName());
 	}
 
 	@Test
 	void straightflash() {
-		for(int i=0; i <5 ; i++) {
-			hand.add(new Card(Suit.SPADE,i+1));
+		for(int i=1; i <=5 ; i++) {
+			hand.add(new Card(Suit.SPADE,i));
 		}
-		assertEquals(Judgement.result(hand),"ストレートフラッシュ");
+		assertEquals(Judgement.result(hand),PokerHand.STRAIGHTFLASH.getName());
 	}
 
 	@Test
 	void royalstraightflash() {
-		int ind = 10;
-		for(int i=0; i <5 ; i++) {
-			if(ind == 14) {
-				ind = 1;
-			}
-			hand.add(new Card(Suit.SPADE,ind));
-			ind++;
+		hand.add(new Card(Suit.SPADE,1));
+		for(int i=10; i <=13 ; i++) {
+			hand.add(new Card(Suit.SPADE,i));
 		}
-		assertEquals(Judgement.result(hand),"ロイヤルストレートフラッシュ");
+		assertEquals(Judgement.result(hand),PokerHand.ROYALSTRAIGHTFLASH.getName());
 	}
 
 	@Test
-	void pair() {
+	void twoPair() {
 		hand.add(new Card(Suit.SPADE,2));
 		hand.add(new Card(Suit.HEART,2));
 		hand.add(new Card(Suit.CLOVER,5));
 		hand.add(new Card(Suit.SPADE,5));
 		hand.add(new Card(Suit.SPADE,8));
-		assertEquals(Judgement.result(hand),"2ペア");
+		assertEquals(Judgement.result(hand),PokerHand.TWOPAIR.getName());
 	}
 
 	@Test
-	void card() {
+	void onewPair() {
+		hand.add(new Card(Suit.SPADE,2));
+		hand.add(new Card(Suit.HEART,2));
+		hand.add(new Card(Suit.CLOVER,5));
+		hand.add(new Card(Suit.SPADE,6));
+		hand.add(new Card(Suit.SPADE,8));
+		assertEquals(Judgement.result(hand),PokerHand.ONEPAIR.getName());
+	}
+
+	@Test
+	void threeCard() {
 		hand.add(new Card(Suit.SPADE,2));
 		hand.add(new Card(Suit.HEART,2));
 		hand.add(new Card(Suit.CLOVER,2));
 		hand.add(new Card(Suit.SPADE,5));
 		hand.add(new Card(Suit.SPADE,8));
-		assertEquals(Judgement.result(hand),"3カード");
+		assertEquals(Judgement.result(hand),PokerHand.THREECARD.getName());
+	}
+
+	@Test
+	void fourCard() {
+		hand.add(new Card(Suit.SPADE,2));
+		hand.add(new Card(Suit.HEART,2));
+		hand.add(new Card(Suit.CLOVER,2));
+		hand.add(new Card(Suit.DIAMOND,2));
+		hand.add(new Card(Suit.SPADE,8));
+		assertEquals(Judgement.result(hand),PokerHand.FOURCARD.getName());
 	}
 
 	@Test
@@ -81,7 +99,17 @@ class JudgeTest {
 		hand.add(new Card(Suit.CLOVER,2));
 		hand.add(new Card(Suit.SPADE,8));
 		hand.add(new Card(Suit.HEART,8));
-		assertEquals(Judgement.result(hand),"フルハウス");
+		assertEquals(Judgement.result(hand),PokerHand.FULLHOUSE.getName());
+	}
+
+	@Test
+	void nothing() {
+		hand.add(new Card(Suit.SPADE,2));
+		hand.add(new Card(Suit.HEART,4));
+		hand.add(new Card(Suit.CLOVER,7));
+		hand.add(new Card(Suit.SPADE,8));
+		hand.add(new Card(Suit.HEART,10));
+		assertEquals(Judgement.result(hand),PokerHand.NOTHING.getName());
 	}
 
 
