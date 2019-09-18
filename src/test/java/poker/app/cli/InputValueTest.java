face321394904f20fb2473f5489e9c1a66ea965b
hiroyuki0210 <h.tamaki@atomitech.jp>
2019-09-18 14:11:15 +0900

    change a expected value according to change of a Exception handling in PlayPoker

--- src/test/java/poker/app/cli/InputValueTest.java
+++ src/test/java/poker/app/cli/InputValueTest.java
@@ -19,15 +19,15 @@ public class InputValueTest {
     @Test
     public void parseInputValueTest_empty() {
         String in = "";
-        int[] out = PlayPoker.parseInput(in);
-        int[] expect = null;
+        int[] out = PlayPoker.parseInputValue(in);
+        int[] expect = {};
         assertThat(out, is(expect));
     }
 
     @Test
     public void parseInputValueTest() {
         String in = "0,2,4";
-        int[] out = PlayPoker.parseInput(in);
+        int[] out = PlayPoker.parseInputValue(in);
         int[] expect = {0, 2, 4};
         assertThat(out, is(expect));
     }
