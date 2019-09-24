ac0c0fcfde71f609d22062fbd11ff2a4bbf1bd40
hiroyuki0210 <h.tamaki@atomitech.jp>
2019-09-24 14:28:35 +0900

    コンピュータの初期状態 & コンピュータの自動交換メソッドの追加

--- src/main/java/poker/domain/service/PokerService.java
+++ src/main/java/poker/domain/service/PokerService.java
@@ -6,30 +6,68 @@ import poker.domain.model.Player;
 import poker.domain.model.hand.Hand;
 
 import java.util.List;
+import java.util.ArrayList;
 
 
 public class PokerService {
 
 	private Deck deck;
-	private Player player;
+	private int numberOfCP;
+	private List<Player> players;
+
+	public PokerService(int numberOfCP) {
+		this.numberOfCP = numberOfCP;
+	}
 
 	public void initialize() {
 		deck = new Deck();
-		List<Card> cards = deck.draw(5);
-		player = new Player(cards);
+		List<Player> players = new ArrayList<>();
+
+		//プレイヤーの初期状態
+		List<Card> playrCards = deck.draw(5);
+		Player player = new Player(playrCards, "プレイヤー");
+		players.add(player);
+
+		//コンピュータの初期状態
+		for(int i = 0; i < numberOfCP; i++) {
+			List<Card> ComputerCards = deck.draw(5);
+			Player computer = new Player(ComputerCards, "コンピュータ" + (i+1));
+			players.add(computer);
+		}
+		this.players = players;
 	}
 
-	public PlayerHandDetail getPlayerHand() {
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public PlayerHandDetail getPlayerHand(Player player) {
 		List<Card> cards = player.getCards();
 		Hand hand = Hand.getHand(cards);
 
 		return new PlayerHandDetail(cards, hand);
 	}
 
-	//カード交換
-	public void exchangeCard(int[] indices) {
+	//プレイヤーのカード交換
+	public void exchangeCard(int[] indices, Player player) {
 		List<Card> newCards = deck.draw(indices.length);
 
 		player.changeCard(indices, newCards);
 	}
+
+	/**
+	 * コンピュータの自動カード交換
+	 * Nothing以外の役が出るまでカード交換する。
+	 */
+	public void autoExchangeCard(Player computer){
+		List<Card> computerCard = computer.getCards();
+		if (Hand.getHand(computerCard).getHandRank() == 9) {
+			int[] changeCards = {0,1,2};
+			List<Card> newCards = deck.draw(3);
+
+			computer.changeCard(changeCards, newCards);
+		}
+	}
+
+	
 }
