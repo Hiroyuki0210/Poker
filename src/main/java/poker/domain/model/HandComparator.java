635b4012cb46a7ac3cfa24929f6c76ea707fcd10
hiroyuki0210 <h.tamaki@atomitech.jp>
2019-09-24 14:30:49 +0900

    順位を決めるためのcomparator

---
+++ src/main/java/poker/domain/model/HandComparator.java
@@ -0,0 +1,20 @@
+package poker.domain.model;
+
+import java.util.Comparator;
+import java.util.List;
+
+import poker.domain.model.hand.Hand;
+
+/**
+ * 手札の強さの評価。
+ * 基本的に数字の小さい順。数字が同じ場合はスートが弱い順。
+ */
+public class HandComparator implements Comparator<Player> {
+	@Override
+	public int compare(Player p1, Player p2) {
+          List<Card> c1 = p1.getCards();
+          List<Card> c2 = p2.getCards();
+
+          return Hand.getHand(c2).compareTo(Hand.getHand(c1));
+	}
+}
